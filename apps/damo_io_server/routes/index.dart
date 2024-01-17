12715a4ff6344e87a08d0b8426a2e20b03facdbf
Damien Le Berrigaud <git@itsdamo.com>
2024-01-17 16:56:20 -0700

    Remove redirect from root

--- apps/damo_io_server/routes/index.dart
+++ apps/damo_io_server/routes/index.dart
@@ -1,6 +1,14 @@
-import 'dart:io';
-
+import 'package:damo_io_server/articles/list_articles.dart';
 import 'package:dart_frog/dart_frog.dart';
+import 'package:html_support/html_support.dart';
 
-Response onRequest(RequestContext context) =>
-    Response(statusCode: HttpStatus.found, headers: {'location': '/articles'});
+Future<Response> onRequest(RequestContext context) async {
+  final listArticles = context.read<ListArticles>();
+  final articles = listArticles.execute();
+  final layout = await context.read<Future<Layout>>();
+
+  return Response(
+    body: layout.render({'main': articles.toHtml()}),
+    headers: {'Content-Type': 'text/html'},
+  );
+}
--- apps/damo_io_server/test/routes/index_test.dart
+++ apps/damo_io_server/test/routes/index_test.dart
@@ -1,19 +1,29 @@
 import 'dart:io';
 
-import 'package:dart_frog/dart_frog.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:damo_io_server/app_dependencies.dart';
 import 'package:test/test.dart';
 
 import '../../routes/index.dart' as route;
-
-final class _MockRequestContext extends Mock implements RequestContext {}
+import '../articles/builders.dart';
+import '../test_support/test_request_context.dart';
 
 void main() {
-  test('GET /', () {
-    final context = _MockRequestContext();
-    final response = route.onRequest(context);
+  test('GET /', () async {
+    final dependencies = AppDependencies.defaults();
+    final context = dependencies.createTestRequestContext();
+    final article = buildArticleRecord(
+      url: 'https://blog.example.com/1',
+      title: 'This is an article',
+    );
 
-    expect(response.statusCode, equals(HttpStatus.found));
-    expect(response.headers['location'], equals('/articles'));
+    dependencies.articles.upsert(article);
+
+    final response = await route.onRequest(context);
+
+    expect(response.statusCode, equals(HttpStatus.ok));
+
+    final body = await response.body();
+    expect(body, contains('<main class="articles">'));
+    expect(body, contains('<h1><a href="https://blog.example.com/1">This is an article</a></h1>'));
   });
 }
