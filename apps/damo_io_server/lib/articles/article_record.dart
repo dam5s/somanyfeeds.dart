a96461de911e5200a2d988bcab6bd7704a162514
Damien Le Berrigaud <git@itsdamo.com>
2024-01-21 18:18:32 -0700

    Remove unnecessary functions

--- apps/damo_io_server/lib/articles/article_record.dart
+++ apps/damo_io_server/lib/articles/article_record.dart
@@ -12,19 +12,4 @@ final class ArticleRecord {
     required this.content,
     required this.publishedAt,
   });
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is ArticleRecord &&
-          runtimeType == other.runtimeType &&
-          feedUrl == other.feedUrl &&
-          url == other.url &&
-          title == other.title &&
-          content == other.content &&
-          publishedAt == other.publishedAt;
-
-  @override
-  int get hashCode =>
-      feedUrl.hashCode ^ url.hashCode ^ title.hashCode ^ content.hashCode ^ publishedAt.hashCode;
 }
